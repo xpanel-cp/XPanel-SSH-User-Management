5c4e25bfa082d78eea742acd3cb883d82cc67298
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2023-08-02 03:03:21 +0330

    Update block_iran.sh

--- block_iran.sh
+++ block_iran.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 #Alireza & Roozemain
-sudo apt-get install curl unzip perl
-sudo apt-get install xtables-addons-common -y
-sudo apt-get install libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl -y
-wait
+
+sudo apt-get update -y
+sudo apt-get -y upgrade
+sudo apt-get install curl unzip perl xtables-addons-common libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl iptables-persistent -y 
 sudo mkdir /usr/share/xt_geoip
-sudo mkdir /usr/lib/xtables-addons/
-sudo mkdir /usr/lib/xtables-addons/xt_geoip_build
-chmod 777 /usr/lib/xtables-addons/xt_geoip_build
-wait
+chmod 755 /usr/lib/xtables-addons/xt_geoip_build
 MON=$(date +"%m")
 YR=$(date +"%Y")
+wget https://download.db-ip.com/free/dbip-country-lite-${YR}-${MON}.csv.gz -O /usr/share/xt_geoip/dbip-country-lite.csv.gz
+gunzip /usr/share/xt_geoip/dbip-country-lite.csv.gz
+/usr/lib/xtables-addons/xt_geoip_build -D /usr/share/xt_geoip/ -S /usr/share/xt_geoip/
+rm /usr/share/xt_geoip/dbip-country-lite.csv
 
-wget https://download.db-ip.com/free/dbip-country-lite-${YR}-${MON}.csv.gz
-cp dbip-country-lite-${YR}-${MON}.csv.gz /usr/share/xt_geoip/
-rm dbip-country-lite-${YR}-${MON}.csv.gz
-gunzip /usr/share/xt_geoip/dbip-country-lite-${YR}-${MON}.csv.gz
-/usr/libexec/xtables-addons/xt_geoip_build -D /usr/share/xt_geoip/ -i /usr/share/xt_geoip/dbip-country-lite-${YR}-${MON}.csv
-modprobe xt_geoip
-lsmod | grep ^xt_geoip
-wait
 echo -e "\n Download Success GEOIP Library  \n"
