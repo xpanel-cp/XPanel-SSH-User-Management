21e9525cc2de74b957c32d5f498527d6955514bf
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2023-08-19 04:07:27 +0330

    Update and rename i3.sh to i4.sh

--- i4.sh
+++ i4.sh
@@ -36,7 +36,7 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 port=$(grep -oE 'Port [0-9]+' /etc/ssh/sshd_config | cut -d' ' -f2)
 
 # Check if MySQL is installed
-if command -v mysql &>/dev/null; then
+if dpkg-query -W -f='${Status}' mariadb-server 2>/dev/null | grep -q "install ok installed"; then
 adminuser=$(mysql -N -e "use XPanel_plus; select username from admins where id='1';")
 adminpass=$(mysql -N -e "use XPanel_plus; select username from admins where id='1';")
 ssh_tls_port=$(mysql -N -e "use XPanel_plus; select tls_port from settings where id='1';")
