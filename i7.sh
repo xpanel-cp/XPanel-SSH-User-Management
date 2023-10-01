d8f19f7b7395f2569a79dafcc9145a7b9f250ee7
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2023-10-02 01:22:57 +0330

    Update and rename i6.sh to i7.sh

--- i7.sh
+++ i7.sh
@@ -11,8 +11,12 @@ if [ "$EUID" -ne 0 ]
 then echo "Please run as root"
 exit
 fi
-cp /var/www/html/app/.env /var/www/html/app/.env_copy
+ENV_FILE="/var/www/html/app/.env"
+COPY_FILE="/var/www/html/.env_copy"
 
+if [ -f "$ENV_FILE" ]; then
+  cp "$ENV_FILE" "$COPY_FILE"
+fi
 # List of supported distributions
 #supported_distros=("Ubuntu" "Debian" "Fedora" "CentOS" "Arch")
 supported_distros=("Ubuntu")
@@ -508,7 +512,7 @@ DEFAULT_DAY=active
 DEFAULT_PORT_DROPBEAR=2083
 DEFAULT_TRAFFIC_BASE=12
 
-if [ -f /var/www/html/app/.env_copy ]; then
+if [ -f /var/www/html/.env_copy]; then
   while IFS= read -r line; do
     key=$(echo "$line" | awk -F'=' '{print $1}')
     value=$(echo "$line" | awk -F'=' '{print $2}')
@@ -528,7 +532,7 @@ if [ -f /var/www/html/app/.env_copy ]; then
     elif [ "$key" = "TRAFFIC_BASE" ]; then
       TRAFFIC_BASE="$value"
     fi
-  done < /var/www/html/app/.env_copy
+  done < /var/www/html/.env_copy
 fi
 
 APP_LOCALE="${APP_LOCALE:-$DEFAULT_APP_LOCALE}"
