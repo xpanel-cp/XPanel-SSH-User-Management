457f0d7412063d51d9c2cf764dd3638d5d92e25e
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2023-10-08 03:53:54 +0330

    Update x5.sh

--- x5.sh
+++ x5.sh
@@ -455,6 +455,33 @@ wait
 ##Restart the webserver server to use new port
 sudo nginx -t
 sudo systemctl reload nginx
+# Getting Proxy Template
+sudo wget -q -O /usr/local/bin/wss https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/wss
+sudo chmod +x /usr/local/bin/wss
+
+# Installing Service
+cat > /etc/systemd/system/wss.service << END
+[Unit]
+Description=Python Proxy XPanel
+Documentation=https://t.me/Xpanelssh
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/wss 8880
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable wss
+systemctl restart wss
 
 chown www-data:www-data /var/www/html/cp/* &
 wait
