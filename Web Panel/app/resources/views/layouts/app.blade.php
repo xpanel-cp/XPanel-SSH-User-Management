d4cde12d5c1f536ed231eb0dc4fb5163a080baa6
Alireza <Alirezadb1994@gmail.com>
2023-10-19 03:41:37 +0330

    XPanel Laravel v3.8.4 fix bug

--- Web Panel/app/resources/views/layouts/app.blade.php
+++ Web Panel/app/resources/views/layouts/app.blade.php
@@ -6,29 +6,29 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Xpanel</title>
-        <!-- [Meta] -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Xpanel</title>
+    <!-- [Meta] -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-        <!-- [Favicon] icon -->
-        <link rel="icon" href="assets/images/xlogo.png" type="image/x-icon">
-        <!-- [Font] Family -->
-        <link rel="stylesheet" href="assets/fonts/inter/inter.css" id="main-font-link" />
+    <!-- [Favicon] icon -->
+    <link rel="icon" href="/assets/images/xlogo.png" type="image/x-icon">
+    <!-- [Font] Family -->
+    <link rel="stylesheet" href="/assets/fonts/inter/inter.css" id="main-font-link" />
 
-        <!-- [Tabler Icons] https://tablericons.com -->
-        <link rel="stylesheet" href="assets/fonts/tabler-icons.min.css" />
-        <!-- [Feather Icons] https://feathericons.com -->
-        <link rel="stylesheet" href="assets/fonts/feather.css" />
-        <!-- [Font Awesome Icons] https://fontawesome.com/icons -->
-        <link rel="stylesheet" href="assets/fonts/fontawesome.css" />
-        <!-- [Material Icons] https://fonts.google.com/icons -->
-        <link rel="stylesheet" href="assets/fonts/material.css" />
-        <!-- [Template CSS Files] -->
-        <link rel="stylesheet" href="assets/css/style-fa-ir.css" id="main-style-link" />
-        <link rel="stylesheet" href="assets/css/style-preset.css" />
-        <link rel="stylesheet" href="assets/css/persian-datepicker.css"/>
+    <!-- [Tabler Icons] https://tablericons.com -->
+    <link rel="stylesheet" href="/assets/fonts/tabler-icons.min.css" />
+    <!-- [Feather Icons] https://feathericons.com -->
+    <link rel="stylesheet" href="/assets/fonts/feather.css" />
+    <!-- [Font Awesome Icons] https://fontawesome.com/icons -->
+    <link rel="stylesheet" href="/assets/fonts/fontawesome.css" />
+    <!-- [Material Icons] https://fonts.google.com/icons -->
+    <link rel="stylesheet" href="/assets/fonts/material.css" />
+    <!-- [Template CSS Files] -->
+    <link rel="stylesheet" href="/assets/css/style-fa-ir.css" id="main-style-link" />
+    <link rel="stylesheet" href="/assets/css/style-preset.css" />
+    <link rel="stylesheet" href="/assets/css/persian-datepicker.css"/>
 </head>
 <body>
 <!-- [ Pre-loader ] start -->
@@ -44,7 +44,7 @@
         <div class="auth-form">
             <div class="card my-5">
                 <div class="card-body">
-            @yield('content')
+                    @yield('content')
                 </div>
             </div>
         </div>
@@ -52,13 +52,13 @@
 </div>
 <!-- [ Main Content ] end -->
 <!-- Required Js -->
-<script src="assets/js/plugins/popper.min.js"></script>
-<script src="assets/js/plugins/simplebar.min.js"></script>
-<script src="assets/js/plugins/bootstrap.min.js"></script>
-<script src="assets/js/fonts/custom-font.js"></script>
-<script src="assets/js/config.js"></script>
-<script src="assets/js/pcoded.js"></script>
-<script src="assets/js/plugins/feather.min.js"></script>
+<script src="/assets/js/plugins/popper.min.js"></script>
+<script src="/assets/js/plugins/simplebar.min.js"></script>
+<script src="/assets/js/plugins/bootstrap.min.js"></script>
+<script src="/assets/js/fonts/custom-font.js"></script>
+<script src="/assets/js/config.js"></script>
+<script src="/assets/js/pcoded.js"></script>
+<script src="/assets/js/plugins/feather.min.js"></script>
 </body>
 <!-- [Body] end -->
 
