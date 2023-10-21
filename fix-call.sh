64fd0f84e291bad0fd075b7995ccd60167e9dbe9
MHitMaN <ghasemi71ir@gmail.com>
2023-10-21 05:29:38 +0330

    Update: Fixcall script to use current compiled fixed badvpn version

--- fix-call.sh
+++ fix-call.sh
@@ -5,17 +5,7 @@ echo -e "\nPlease input UDPGW Port ."
 printf "Default Port is \e[33m${udpport}\e[0m, let it blank to use this Port: "
 read udpport
 
-
-apt update -y
-apt install git cmake -y
-git clone https://github.com/ambrop72/badvpn.git /root/badvpn
-mkdir /root/badvpn/badvpn-build
-cd  /root/badvpn/badvpn-build
-cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1 &
-wait
-make &
-wait
-cp udpgw/badvpn-udpgw /usr/local/bin
+sudo wget -4 -O /usr/local/bin https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/badvpn-udpgw
 cat >  /etc/systemd/system/videocall.service << ENDOFFILE
 [Unit]
 Description=UDP forwarding for badvpn-tun2socks
