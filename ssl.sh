dad7b45338c786af29517ea712cdd87e857e1197
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2024-01-09 02:26:31 +0330

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -17,6 +17,7 @@ server {
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
+        fastcgi_param PHP_VALUE "memory_limit=4096M";
     }
     location ~ /\.ht {
         deny all;
@@ -53,6 +54,7 @@ server {
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
+        fastcgi_param PHP_VALUE "memory_limit=4096M";
     }
 
     location ~ /\.ht {
@@ -89,6 +91,7 @@ server {
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
+        fastcgi_param PHP_VALUE "memory_limit=4096M";
     }
     location ~ /\.ht {
         deny all;
