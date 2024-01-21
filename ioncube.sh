731ef52e5f5d9b22c07b86034fa3fdf42165b0d0
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2024-01-21 13:24:20 +0330

    Update ioncube.sh

--- ioncube.sh
+++ ioncube.sh
@@ -7,4 +7,8 @@ PHPVERSION=$(php -i | grep /.+/php.ini -oE | sed 's/[^0-9.]*//g')
 echo "zend_extension = /usr/local/ioncube/ioncube_loader_lin_${PHPVERSION}.so" > /etc/php/${PHPVERSION::-1}/fpm/conf.d/00-ioncube.ini
 echo "zend_extension = /usr/local/ioncube/ioncube_loader_lin_${PHPVERSION}.so" > /etc/php/${PHPVERSION::-1}/cli/conf.d/00-ioncube.ini
 
+PHP_INI_PATH="/etc/php/8.1/fpm/php.ini"
+ZEND_EXTENSION_PATH="/usr/local/ioncube/ioncube_loader_lin_8.1.so"
+grep -q "^zend_extension" $PHP_INI_PATH && sed -i "s@^zend_extension.*@zend_extension = $ZEND_EXTENSION_PATH@" $PHP_INI_PATH || echo "zend_extension = $ZEND_EXTENSION_PATH" >> $PHP_INI_PATH
+sudo systemctl restart php8.1-fpm
 systemctl restart nginx
