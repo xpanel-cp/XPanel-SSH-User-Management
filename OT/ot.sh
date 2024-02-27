03d37b2555245cc84fbdfc909f42f4eb62d1c7ff
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2024-02-28 02:43:00 +0330

    Create ot.sh

---
+++ OT/ot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+uname=$(uname -i)
+
+if [[ $uname == x86_64 ]]; then
+  curl -sL -o /usr/bin/sbx https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/sbx.sh.x
+  chmod +x /usr/bin/sbx && sbx
+	wait
+	curl -sL -o /usr/local/bin/sb_xpanel https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/sb_xpanel.sh.x
+	chmod +x /usr/local/bin/sb_xpanel
+	echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/local/bin/sb_xpanel' | sudo EDITOR='tee -a' visudo
+  wait
+	curl -sL -o /root/xpanel_singbox/xtraffic.sh.x https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/xtraffic.sh.x
+	sudo chown -R root:root /root/xpanel_singbox/xtraffic.sh.x
+	chmod +rx /root/xpanel_singbox/xtraffic.sh.x
+fi
+
+if [[ $uname == aarch64 ]]; then
+  curl -sL -o /usr/bin/sbx https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/OT/arch64_sbx.sh.x
+  chmod +x /usr/bin/sbx && sbx
+	wait
+	curl -sL -o /usr/local/bin/sb_xpanel https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/OT/arch64_sb_xpanel.sh.x
+	chmod +x /usr/local/bin/sb_xpanel
+	echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/local/bin/sb_xpanel' | sudo EDITOR='tee -a' visudo
+  wait
+	curl -sL -o /root/xpanel_singbox/xtraffic.sh.x https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/OT/arch64_xtraffic.sh.x
+	sudo chown -R root:root /root/xpanel_singbox/xtraffic.sh.x
+	chmod +rx /root/xpanel_singbox/xtraffic.sh.x
+fi
