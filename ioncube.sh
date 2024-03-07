425fc5c344442be5229e3e063d9962bafa83ad42
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2024-03-07 16:13:09 +0330

    Update ioncube.sh

--- ioncube.sh
+++ ioncube.sh
@@ -1,7 +1,14 @@
+uname=$(uname -i)
+if [[ $uname == x86_64 ]]; then
 wget -4 https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz -C /usr/local
 sudo rm -rf ioncube_loaders_lin_x86-64.tar.gz
-
+fi
+if [[ $uname == aarch64 ]]; then
+wget -4 https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_aarch64.tar.gz
+sudo tar xzf ioncube_loaders_lin_aarch64.tar.gz -C /usr/local
+sudo rm -rf ioncube_loaders_lin_aarch64.tar.gz
+fi
 PHPVERSION=$(php -i | grep /.+/php.ini -oE | sed 's/[^0-9.]*//g')
 
 echo "zend_extension = /usr/local/ioncube/ioncube_loader_lin_${PHPVERSION}.so" > /etc/php/${PHPVERSION::-1}/fpm/conf.d/00-ioncube.ini
