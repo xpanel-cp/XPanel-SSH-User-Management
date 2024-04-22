af960854d997aa1cbe63896559bb3393766808f7
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2024-04-22 04:38:10 +0330

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -79,6 +79,7 @@ function show_menu() {
 	echo -e " ${BLUE}[10]${NC} ${ORANGE}•${NC} ${WHITE}Install WordPress${NC}"
 	echo -e " ${BLUE}[11]${NC} ${ORANGE}•${NC} ${WHITE}Fix Call (UDPGW)${NC}"
 	echo -e " ${BLUE}[12]${NC} ${ORANGE}•${NC} ${WHITE}Sing-box${NC}"
+        echo -e " ${BLUE}[13]${NC} ${ORANGE}•${NC} ${WHITE}Install BBR${NC}"
 	echo -e "$BLUE•────────────────────────────────────────────────────────────────┘${NC}"
 	echo -e "$BLUE┌─────────────────────────┐${NC}"
 	echo -e "$BLUE│$NC ${BLUE}[0]${NC} ${ORANGE}•${NC} ${RED}Exit${NC}$NC"
@@ -172,7 +173,10 @@ connect = 0.0.0.0:$sshport
         ;; 
         12)
         bash <(curl -Ls https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/OT/singbox.sh --ipv4)
-        ;; 		
+        ;;
+	13)
+        bash <(curl -Ls https://raw.githubusercontent.com/teddysun/across/master/bbr.sh --ipv4)
+        ;;
         0)
             echo "Exiting the menu."
             exit 0
