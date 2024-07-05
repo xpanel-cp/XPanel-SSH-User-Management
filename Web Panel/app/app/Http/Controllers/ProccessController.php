3807e2ff16fbe98428333e06d1ef4515c9761461
xpanel-cp <xpanel042@gmail.com>
2024-07-05 23:17:46 +0330

    XPanel Laravel v3.9.8

--- Web Panel/app/app/Http/Controllers/ProccessController.php
+++ Web Panel/app/app/Http/Controllers/ProccessController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Singbox;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\Process\Process;
@@ -15,6 +16,7 @@ class ProccessController extends Controller
     public function prcs_active_user()
     {
         $users = Users::where('status','active')->get();
+        $s_users = Singbox::where('status','active')->get();
 
         $processes = [];
 
@@ -30,6 +32,27 @@ class ProccessController extends Controller
             $process2->setInput("{$password}\n{$password}\n");
             $process2->start();
             $processes[] = $process2;
+        }
+
+        foreach ($s_users as $user) {
+            $port = $user->port_sb;
+            $user = Singbox::where('port_sb',$port)->first();
+            $jsonData = json_decode($user->detail_sb, true);
+            $sid=$jsonData['sid'];
+            $uuid=$jsonData['uuid'];
+            $protocol=$user->protocol_sb;
+            $name=$user->name;
+            $multiuser=$user->multiuser;
+            $validatedData = [
+                'port'=>$port,
+                'protocol'=>$protocol,
+                'sid'=>$sid,
+                'uuid'=>$uuid,
+                'name'=>$name,
+                'multiuser'=>$multiuser
+            ];
+
+            ProController::active_singbox($validatedData);
         }
 
         foreach ($processes as $process) {
