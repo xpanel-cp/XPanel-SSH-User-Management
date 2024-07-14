ce7ab5644be3988a3da4462e6160251ac2d1ee5c
xpanel-cp <xpanel042@gmail.com>
2024-07-15 00:14:56 +0330

    Fixed Bug IP Rotation

--- Web Panel/app/app/Http/Controllers/SettingsController.php
+++ Web Panel/app/app/Http/Controllers/SettingsController.php
@@ -478,6 +478,7 @@ class SettingsController extends Controller
                 $protocol=$user['protocol_sb'];
                 $detail_sb=$user['detail_sb'];
                 $name=$user['name'];
+                $multiuser=$user['multiuser'];
                 $check_user = Singbox::where('port_sb',$port)->count();
                 if ($check_user > 0) {
                     $jsonData = json_decode($detail_sb, true);
@@ -488,7 +489,8 @@ class SettingsController extends Controller
                         'protocol'=>$protocol,
                         'sid'=>$sid,
                         'uuid'=>$uuid,
-                        'name'=>$name
+                        'name'=>$name,
+                        'multiuser'=>$multiuser,
                     ];
 
                     ProController::active_singbox($validatedData);
